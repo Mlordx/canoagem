e32c367f2140a2acb5d61aa61eafdc963ac5ecfa
Victor Portella <visportella@gmail.com>
2013-05-19 22:16:26 -0300

    terreno.c comentado

--- terreno.c
+++ terreno.c
@@ -8,6 +8,12 @@ struct terreno{
 };
 
 
+/*
+Função que retorna um novo Terreno com o tipo especificado
+e com a velocidade especificada.
+
+*/
+
 Terreno novoTerreno(char tipo, float velocidade)
 {
     Terreno terrTemp = mallocSafe(sizeof(Terreno*));
@@ -18,26 +24,57 @@ Terreno novoTerreno(char tipo, float velocidade)
     return terrTemp;
 }
 
+
+/*
+
+Libera a memória alocada pelo terreno passado como argumento.
+
+*/
+
 void freeTerreno(Terreno temp)
 {
     free(temp);
 }
 
 
+
+/*
+Retorna a velocidade do terreno passado.
+
+*/
+
 float getVelocidade(Terreno terr)
 {
     return terr->velocidade;
 }
+
+/*
+
+Retorna o tipo do terreno passado.
+
+*/
 
 char getTipo(Terreno terr)
 {
     return terr->tipo;
 }
 
+
+/*
+
+Define a velocidade do terreno passado como argumento.
+
+*/
+
 void setVelocidade(Terreno terr, float vel)
 {
     terr->velocidade = vel;
 }
+
+/*
+Define o tipo do terreno passado como argumento
+
+*/
 
 void setTipo(Terreno terr, char tipo)
 {
